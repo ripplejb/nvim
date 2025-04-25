d74d7d5b6e8db3d12ba7cbee73b23599d5ac4f60
Ripal Barot <ripplejb@hotmail.com>
2025-04-25 08:30:39 -0500

    added ollama plug in and set up for deepseek-coder

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -1,22 +1,11 @@
 return {
   "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
 
-  -- All the user commands added by the plugin
   cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
 
   keys = {
-    -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-    {
-      "<leader>oo",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
-    },
-
-    -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
+    { "<leader>oo", ":<c-u>lua require('ollama').prompt()<cr>", desc = "ollama prompt", mode = { "n", "v" } },
     {
       "<leader>oG",
       ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
@@ -25,8 +14,23 @@ return {
     },
   },
 
-  -- -@type Ollama.Config
   opts = {
-    -- your configuration overrides
+    model = "deepseek-coder",
+    url = "http://127.0.0.1:11434",
+    serve = {
+      on_start = false,
+      command = "ollama",
+      args = { "serve" },
+      stop_command = "pkill",
+      stop_args = { "-SIGTERM", "ollama" },
+    },
+    prompts = {
+      Sample_Prompt = {
+        prompt = "This is a sample prompt that receives $input and $sel(ection), among others.",
+        input_label = "> ",
+        model = "deepseek-coder",
+        action = "display",
+      },
+    },
   },
 }
